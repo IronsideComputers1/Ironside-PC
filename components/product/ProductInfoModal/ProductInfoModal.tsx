5488cacc6d21ea1124f1316ae574eca18936c175
Rafael Melo <16295402+rsmelo92@users.noreply.github.com>
2024-11-26 12:37:07 -0300

    feat: add ProductInfoModal new layout and logic (#8)
    * chore: checking the cart logic
    
    * chore: add out of stock logic
    
    * chore: first changes on logic
    
    * feat: implement cart behavior
    
    * chore: remove comments
    
    * chore: fix lint and header
    
    * chore: fix justify-evenly not working
    
    * chore: fix gap not working in prod
    
    * chore: Add save my build
    
    * Fix conflicts
    
    * feat: add scroller
    
    * chore: scroller working properly
    
    * chore: increase bottom distance
    
    * chore: fix light mode with new hooks
    
    * chore: lightmode fixes
    
    * chore: more light migrations
    
    * chore: last light mode changes
    
    * chore: fix lint
    
    * feat: new ProductInfo Modal
    
    * chore: add product modal
    
    * chore: remove comments

--- components/product/ProductInfoModal/ProductInfoModal.tsx
+++ components/product/ProductInfoModal/ProductInfoModal.tsx
@@ -18,10 +18,10 @@ const ProductInfoModal: FC<Props> = ({
   productImages,
   button,
   heading,
-  open,
+  open = false,
   stock,
 }: any) => {
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(open);
   const [nav1, setNav1] = useState<any>(null)
   const [nav2, setNav2] = useState<any>(null)
   const [slider1, setSlider1] = useState(null)
@@ -31,7 +31,6 @@ const ProductInfoModal: FC<Props> = ({
     setNav1(slider1)
     setNav2(slider2)
   })
-  const ref = useRef() as React.MutableRefObject<HTMLDivElement>
   const settingsMain = {
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -85,12 +84,22 @@ const ProductInfoModal: FC<Props> = ({
         </span>
       </div>
       <Portal>
-        <div>
+        <div className='relative'>
           <div role="dialog">
             {showModal ? (
               <>
-                <div className="modal product-modal" data-lenis-prevent>
-                  <div className="w-100">
+                <div className="modal customizer-prod-modal" data-lenis-prevent>
+                  <div className='w-full flex items-center justify-between text-gray-500 border-b border-primary h-12 pl-8'>
+                    <p className='m-0 text-basicDark font-medium font-Inconsolata'>PRODUCT_OVERVIEW.EXE</p>
+                    <button
+                      onClick={() => setShowModal(false)}
+                      aria-label="Close panel"
+                      className="m-6 border w-7 h-7 px-2 rounded-full flex items-center justify-center mr-3 hover:text-gray-500 transition ease-in-out duration-150 focus:outline-none right-0 top-0"
+                    >
+                      <Cross className="h-3 w-3 fill-current" />
+                    </button>
+                  </div>
+                  <div className="modal-content w-full grid grid-cols-2 pb-10 pl-13 pt-2">
                     <div className="product-thumbnail flex flex-direction justify-center items-center">
                       <Slider
                         className="product-thumbnail-main"
@@ -136,20 +145,14 @@ const ProductInfoModal: FC<Props> = ({
                         ''
                       )}
                     </div>
-                    <button
-                      onClick={() => setShowModal(false)}
-                      aria-label="Close panel"
-                      className="modal-close hover:text-gray-500 transition ease-in-out duration-150 focus:outline-none absolute right-0 top-0 m-6"
-                    >
-                      <Cross className="h-6 w-6" />
-                    </button>
-                    <div className="product-thumbnail-content">
-                      <div className="product-thumbnail-content-heading">
+                    <div className="overflow-auto h-auto pt-12 pr-20">
+                      <div>
                         <h2>{heading}</h2>
                       </div>
                       {text ? (
-                        <div className="product-description" data-lenis-prevent>
-                          <div dangerouslySetInnerHTML={{ __html: text }} />
+                        <div data-lenis-prevent>
+                          <p className='font-mono capitalize mt-8 mb-6 text-xl text-gray-300'>Description</p>
+                          <div className='font-mono text-gray-300 leading-5' dangerouslySetInnerHTML={{ __html: text }} />
                         </div>
                       ) : (
                         ''
