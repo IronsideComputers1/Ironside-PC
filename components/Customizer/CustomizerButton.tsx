876b11703f815a3d359f78865aa377b7f47c3be2
Rafael Melo <rsmelo_@hotmail.com>
2024-12-13 17:36:09 -0300

    chore: add dark mode

--- components/Customizer/CustomizerButton.tsx
+++ components/Customizer/CustomizerButton.tsx
@@ -13,27 +13,28 @@ interface CustomizerButtonProps {
 export const CustomizerButton: React.FC<CustomizerButtonProps> = ({ 
   onClick,
   isLoading,
+  isDisabled,
   isIncompatible,
   children,
 }) => {
   const classes = classNames(
-    "text-base leading-4 font-secondary font-normal text-center rounded-full min-w-[178px] py-5 px-2.5 tracking-tight cursor-pointer transition-all duration-1000 relative bg-button-color uppercase",
-    "",
+    "text-base leading-4 font-secondary font-normal text-center rounded-full min-w-[178px] py-5 px-2.5 tracking-tight cursor-pointer transition-all duration-1000 relative uppercase",
     "disabled:cursor-not-allowed disabled:bg-transparent disabled:text-secondary disabled:opacity-80",
+    "dark:bg-button-color dark:text-white ",
     {
-      "text-white border border-gray-800 hover:bg-theme-bg hover:border-secondary": !isIncompatible,
+      "border border-gray-800 hover:bg-theme-bg hover:border-secondary dark:border border-gray-800 dark:text-white": !isIncompatible,
       "text-[#FF7070] border-[1px] border-[#FF7070] filter-none": isIncompatible,
     },
   )
   return (
-    <div onClick={onClick} className={classes}>
+    <button onClick={onClick} className={classes} disabled={isDisabled}>
       {children}
       {isLoading && (
         <span className='ml-3'>
           <LoadingDots />
         </span>
       )}
-    </div>
+    </button>
   );
 };
 
